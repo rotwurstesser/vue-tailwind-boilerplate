bb7a5719826b038ba6302d9fd8ae912ce8ddd0c6
rotwurstesser <raphael.antonietti@liip.ch>
2019-08-20 19:17:33 +0200

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,23 @@
 module.exports = {
-    "plugins": [
-      require('tailwindcss')('tailwind.js'),
-      require('autoprefixer')(),
-    ]
-  }
+  theme: {
+    colors: {
+      indigo: '#5c6ac4',
+      green: '#007ace',
+      blue: '#007ace',
+      red: '#de3618',
+      cyan: '#1ea274',
+      gray: '#777777',
+      white: '#FFFFFF',
+      semitransparent: 'rgba(1,1,1,0.5)',
+      iceblue: '#4174e7'
+    },
+    extend: {}
+  },
+  variants: {
+    margin: ['responsive', 'first', 'hover', 'focus']
+  },
+  "plugins": [
+    require('tailwindcss')('tailwind.js'),
+    require('autoprefixer')(),
+  ]
+};
